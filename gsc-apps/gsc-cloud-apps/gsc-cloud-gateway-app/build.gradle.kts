3916a3354a8c6a7b004e89ba0a0fe3dd328e44b6
liangweidong <liangweidong@hxl.com>
2025-07-19 18:04:59 +0800

    gateway

--- gsc-apps/gsc-cloud-apps/gsc-cloud-gateway-app/build.gradle.kts
+++ gsc-apps/gsc-cloud-apps/gsc-cloud-gateway-app/build.gradle.kts
@@ -3,11 +3,16 @@ plugins {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-config")
     implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-discovery")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
     implementation("com.alibaba.cloud:spring-cloud-alibaba-sentinel-gateway")
     implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-sentinel")
     implementation("com.alibaba.csp:sentinel-datasource-nacos")
+    implementation("com.github.xiaoymin:knife4j-gateway-spring-boot-starter")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 }
---
+++ gsc-apps/gsc-cloud-apps/gsc-cloud-gateway-app/src/main/java/com/lwd/gsc/gateway/config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package com.lwd.gsc.gateway.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    // 关键：添加 @LoadBalanced 注解，让 WebClient 支持 lb:// 协议
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
+}
